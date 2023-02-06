f8aabe63b8eef8fa815d304d8ac30bc3199f2704
JasonGuo1 <1515893624@qq.com>
2023-02-06 23:21:02 +0800

    fix: (package.json)更改scripts中build内容

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="avatar_test">
+  <div class="input_test"></div>
+
+  <!-- <div class="avatar_test">
     <div class="c1">
       <avatar :src="imgUrl" size="small" alt="小头像" />
       <avatar :src="imgUrl" size="default" />
@@ -34,14 +36,14 @@
       <avatar size="large" background-color="gray">Hi</avatar>
       <avatar :size="100" background-color="gray">Hi</avatar>
     </div>
+  </div> -->
 
-    <!-- <ButtonPrimary></ButtonPrimary>
-    <ButtonPrimary type="primary">主要按钮</ButtonPrimary>
-    <ButtonPrimary type="info">信息按钮</ButtonPrimary>
-    <ButtonPrimary type="success">成功按钮</ButtonPrimary>
-    <ButtonPrimary type="warning">警告按钮</ButtonPrimary>
-    <ButtonPrimary type="danger">危险按钮</ButtonPrimary> -->
-  </div>
+  <!-- <ButtonPrimary></ButtonPrimary>
+      <ButtonPrimary type="primary">主要按钮</ButtonPrimary>
+      <ButtonPrimary type="info">信息按钮</ButtonPrimary>
+      <ButtonPrimary type="success">成功按钮</ButtonPrimary>
+      <ButtonPrimary type="warning">警告按钮</ButtonPrimary>
+      <ButtonPrimary type="danger">危险按钮</ButtonPrimary> -->
 </template>
 
 <style lang="scss" scoped>
@@ -70,6 +72,6 @@ nav {
 <script lang="ts" setup>
 // import ButtonPrimary from './views/button/button.vue';
 
-import avatar from './views/avatar/avatar.vue';
-import imgUrl from './assets/image/avatar.png';
+// import avatar from './views/avatar/avatar.vue';
+// import imgUrl from './assets/image/avatar.png';
 </script>
---
+++ src/views/button/button.vue
@@ -0,0 +1,141 @@
+<!-- // button.vue -->
+<template>
+  <button
+    class="simple-button"
+    :class="[type ? `simple-button--${type} text-color` : '']"
+  >
+    <span><slot>这是style样式按钮</slot></span>
+  </button>
+  <button
+    class="my-button"
+    :class="[type ? `my-button-${type} text-color` : '']"
+  >
+    <span><slot>这是使用scss文件的button</slot></span>
+  </button>
+  <br />
+</template>
+
+<script lang="ts" setup>
+// 工具函数，用于判断传入的值是否符合条件
+// import { oneOf } from "../../utils/assist";
+import { defineProps } from 'vue';
+
+defineProps({
+  type: {
+    type: String,
+    default: 'primary',
+  },
+});
+
+// export default {
+//   name: "ButtonPrimary",
+//   data() {
+//     return {};
+//   },
+// props: {
+//   type: {
+//     // validator(value: any) {
+//       return oneOf(value, [
+//         "primary",
+//         "info",
+//         "success",
+//         "warning",
+//         "text",
+//         "error",
+//       ]);
+//     },
+//     type: String,
+//     default: "primary",
+//   },
+// },
+// };
+</script>
+<!-- <script>
+  export default {
+    name: "ButtonPrimary",
+    data() {
+      return {};
+    },
+  };
+  </script> -->
+<style lang="scss">
+@import '../../styles/index';
+
+.my-button {
+  @include button-wrapper;
+}
+
+.simple-button {
+  /* 设置宽度自适应 */
+  width: auto {
+
+  }
+  height: 35px {
+
+  }
+
+  /* 将按钮左右边距调大一些显得美观 */
+  padding: 0 10px {
+
+  }
+
+  /* 居中 */
+  text-align: center {
+
+  }
+  justify-content: center {
+
+  }
+  align-items: center {
+
+  }
+
+  /* 边框不显示 */
+  border: none {
+
+  }
+
+  /* 圆角框 */
+  border-radius: 3px {
+
+  }
+
+  /* 盒子阴影 */
+  box-shadow: 0 0 3px gray {
+
+  }
+  font-family: inherit {
+
+  }
+
+  /* 鼠标变为小手标识 */
+  cursor: pointer {
+
+  }
+  font-size: 14px;
+}
+
+.simple-button--primary {
+  background-color: rgb(74 130 212);
+}
+
+.simple-button--info {
+  background-color: rgb(163 191 233);
+}
+
+.simple-button--success {
+  background-color: rgb(92 218 180);
+}
+
+.simple-button--warning {
+  background-color: rgb(221 219 77);
+}
+
+.simple-button--danger {
+  background-color: rgb(233 56 56);
+}
+
+.text-color {
+  color: rgb(134, 84, 84);
+}
+</style>
