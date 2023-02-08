1745aab5ba02c08ed48d93688be8175cee4e4433
ValeriaWong <763717425@qq.com>
2023-02-08 22:31:39 +0800

    feat: (button)增加大小形状属性控制

--- src/App.vue
+++ src/App.vue
@@ -41,19 +41,53 @@
   </div>
 
   <div>
-    <ButtonPrimary></ButtonPrimary>
-    <ButtonPrimary type="primary">主要按钮</ButtonPrimary>
-    
-    <ButtonPrimary type="secondary">次级按钮</ButtonPrimary>
-    <ButtonPrimary :loading="!isLoading" type="primary">带loading的主要按钮</ButtonPrimary>
-    <ButtonPrimary :loading="!isLoading" type="secondary">带loading的次级按钮</ButtonPrimary>
-    <ButtonPrimary :loading="!isLoading" type="revert">带loading的回退按钮</ButtonPrimary>
-    <!-- component event should use emit, default event like 'click' may block -->
-    <button @click="isLoading = !isLoading">Switch Loading</button>
-    <ButtonPrimary type="revert">回退按钮</ButtonPrimary>
+    <div>
+      <ButtonPrimary round="square">方角</ButtonPrimary>
+      <ButtonPrimary round="round">圆角</ButtonPrimary>
+      <ButtonPrimary round="other1">平行四边形</ButtonPrimary>
+      <ButtonPrimary round="other2">椭圆</ButtonPrimary>
+      <ButtonPrimary round="other3">平行四边形</ButtonPrimary>
+      <ButtonPrimary round="other4">活力标题</ButtonPrimary>
+    </div>
+    <p></p>
+    <div>
+      <ButtonPrimary type="primary" size="S">主要按钮</ButtonPrimary>
+      <ButtonPrimary type="primary" size="M">主要按钮</ButtonPrimary>
+      <ButtonPrimary type="primary" size="L">主要按钮</ButtonPrimary>
+    </div>
+    <p></p>
+    <div>
+      <ButtonPrimary type="secondary" size="S">次级按钮</ButtonPrimary>
+      <ButtonPrimary type="secondary" size="M">次级按钮</ButtonPrimary>
+      <ButtonPrimary type="secondary" size="L">次级按钮</ButtonPrimary>
+    </div>
+    <p></p>
+    <div>
+      <ButtonPrimary type="revert" size="S">回退按钮</ButtonPrimary>
+      <ButtonPrimary type="revert" size="M">回退按钮</ButtonPrimary>
+      <ButtonPrimary type="revert" size="L">回退按钮</ButtonPrimary>
+    </div>
+    <p></p>
+    <div>
+      <button @click="isLoading = !isLoading">Switch Loading</button>
+      <br />
+      <ButtonPrimary :loading="!isLoading" type="primary"
+        >带loading的主要按钮</ButtonPrimary
+      >
+      <br />
+      <ButtonPrimary :loading="!isLoading" type="secondary"
+        >带loading的次级按钮</ButtonPrimary
+      >
+      <br />
+      <ButtonPrimary :loading="!isLoading" type="revert"
+        >带loading的回退按钮</ButtonPrimary
+      >
+      <!-- component event should use emit, default event like 'click' may block -->
+    </div>
+    <p></p>
     <ButtonPrimary type="warning">警告按钮</ButtonPrimary>
     <ButtonPrimary type="danger">危险按钮</ButtonPrimary>
-  </div> 
+  </div>
 </template>
 
 <style lang="scss" scoped>
@@ -86,5 +120,5 @@ import ButtonPrimary from './views/button/button.vue';
 import avatar from './views/avatar/avatar.vue';
 import imgUrl from './assets/image/avatar.png';
 // import MyInput from './views/input/input.vue';
-const isLoading = ref(true)
+const isLoading = ref(true);
 </script>
--- src/views/button/button.vue
+++ src/views/button/button.vue
@@ -8,18 +8,16 @@
   </button> -->
   <button
     class="my-button"
-    :class="[type ? `my-button-${type} text-color` : '']"
+    :class="[type ? `my-button-${type}` : '', size ? `my-button-${size}` : '',round ? `my-button--${round}` : '']"
   >
     <span v-if="loading"
       ><img
         src="../../assets/loading.png"
         alt="loading"
-        
       /><!--  put loading icon to this section --></span
     >
     <span v-else><slot>这是使用scss文件的button</slot></span>
   </button>
-  <br />
 </template>
 
 <script lang="ts" setup>
@@ -32,10 +30,23 @@ defineProps({
     type: String,
     default: 'primary',
   },
+  size: {
+    type: String,
+    default: 'M',
+  },
   loading: {
     type: Boolean, // delete default value
   },
+  round: {
+    type: String,
+    default: 'square',
+  },
+  iconPlacement: {
+    type: String,
+    default: 'left',
+  },
 });
+
 // export default {
 //   name: "ButtonPrimary",
 //   data() {
@@ -73,78 +84,4 @@ defineProps({
 .my-button {
   @include button-wrapper;
 }
-
-.simple-button {
-  /* 设置宽度自适应 */
-  width: auto {
-
-  }
-  height: 35px {
-
-  }
-
-  /* 将按钮左右边距调大一些显得美观 */
-  padding: 0 10px {
-
-  }
-
-  /* 居中 */
-  text-align: center {
-
-  }
-  justify-content: center {
-
-  }
-  align-items: center {
-
-  }
-
-  /* 边框不显示 */
-  border: none {
-
-  }
-
-  /* 圆角框 */
-  border-radius: 3px {
-
-  }
-
-  /* 盒子阴影 */
-  box-shadow: 0 0 3px gray {
-
-  }
-  font-family: inherit {
-
-  }
-
-  /* 鼠标变为小手标识 */
-  cursor: pointer {
-
-  }
-  font-size: 14px;
-}
-
-.simple-button--primary {
-  background-color: rgb(74 130 212);
-}
-
-.simple-button--info {
-  background-color: rgb(163 191 233);
-}
-
-.simple-button--success {
-  background-color: rgb(92 218 180);
-}
-
-.simple-button--warning {
-  background-color: rgb(221 219 77);
-}
-
-.simple-button--danger {
-  background-color: rgb(233 56 56);
-}
-
-// .text-color {
-//   color: rgb(250, 250, 250);
-// }
 </style>
