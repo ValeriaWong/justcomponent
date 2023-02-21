f0c590e7f3da6def6de71746a226182e6437e6ca
ValeriaWong <763717425@qq.com>
2023-02-22 00:08:57 +0800

    chore: (app.vue)更改组件命名使其保持一致性，大驼峰JustComponentName

--- src/App.vue
+++ src/App.vue
@@ -1,181 +1,179 @@
 <template>
   <Space wrap>
-    <div class="btn-able-wrapper" v-if="type == 'btnAble'">
-      <div>
-        <ButtonPrimary type="primary">主要按钮</ButtonPrimary>
-        <ButtonPrimary type="secondary">次级按钮</ButtonPrimary>
-        <ButtonPrimary type="revert">回退按钮</ButtonPrimary>
-        <ButtonPrimary type="icon"></ButtonPrimary>
-        <ButtonPrimary type="icon" icon="clear"></ButtonPrimary>
-        <ButtonPrimary type="icon" icon="download"></ButtonPrimary>
-      </div>
+    <div class="btn-wrapper">
+      <div class="btn-able-wrapper" v-if="type == 'btnAble'">
+        <div>
+          <JustButton type="primary">主要按钮</JustButton>
+          <JustButton type="secondary">次级按钮</JustButton>
+          <JustButton type="revert">回退按钮</JustButton>
+          <JustButton type="icon"></JustButton>
+          <JustButton type="icon" icon="clear"></JustButton>
+          <JustButton type="icon" icon="download"></JustButton>
+        </div>
 
-      <ButtonPrimary type="danger">危险按钮</ButtonPrimary>
+        <JustButton type="danger">危险按钮</JustButton>
+      </div>
+      <div class="btn-disable-wrapper" v-if="type == 'btnDisable'">
+        <div>
+          <JustButton disabled>默认按钮</JustButton>
+          <JustButton type="primary" disabled>主要按钮</JustButton>
+          <JustButton type="secondary" disabled>次级按钮</JustButton>
+          <JustButton type="revert" disabled>回退按钮</JustButton>
+          <JustButton type="warning" disabled>警告按钮</JustButton>
+          <JustButton type="danger" disabled>危险按钮</JustButton>
+        </div>
+
+        <div>
+          <JustButton plain disabled>朴素按钮</JustButton>
+          <JustButton type="primary" plain disabled>主要按钮</JustButton>
+          <JustButton type="secondary" plain disabled>成功按钮</JustButton>
+          <JustButton type="revert" plain disabled>信息按钮</JustButton>
+          <JustButton type="warning" plain disabled>警告按钮</JustButton>
+          <JustButton type="danger" plain disabled>危险按钮</JustButton>
+        </div>
+      </div>
+      <div v-if="type == 'btnIcon'">
+        <JustButton type="icon"></JustButton>
+        <JustButton type="icon" icon="clear"></JustButton>
+        <JustButton type="icon" icon="download"></JustButton>
+      </div>
+      <div>
+        <div class="btn-shape-wrapper" v-if="type == 'btnShape'">
+          <div>
+            <JustButton round="square">方角</JustButton>
+            <JustButton round="round">圆角</JustButton>
+            <JustButton round="other1">平行四边形</JustButton>
+            <JustButton round="other2">椭圆</JustButton>
+            <JustButton round="other3">平行四边形</JustButton>
+            <JustButton round="other4">活力标题</JustButton>
+          </div>
+        </div>
+        <div class="btn-size-wrapper" v-if="type == 'btnSize'">
+          <div>
+            <JustButton type="primary" size="S">主要按钮</JustButton>
+            <JustButton type="primary" size="M">主要按钮</JustButton>
+            <JustButton type="primary" size="L">主要按钮</JustButton>
+          </div>
+
+          <div>
+            <JustButton type="secondary" size="S">次级按钮</JustButton>
+            <JustButton type="secondary" size="M">次级按钮</JustButton>
+            <JustButton type="secondary" size="L">次级按钮</JustButton>
+          </div>
+
+          <div>
+            <JustButton type="revert" size="S">回退按钮</JustButton>
+            <JustButton type="revert" size="M">回退按钮</JustButton>
+            <JustButton type="revert" size="L">回退按钮</JustButton>
+          </div>
+        </div>
+        <div class="btn-loading-wrapper" v-if="type == 'btnLoading'">
+          <div style="display: flex">
+            <!--防止点击后更换位置，需要在父级块状元素处增加style="display: flex;"-->
+            <JustButtonLoading type="primary"
+              >点击切换loading的主要按钮</JustButtonLoading
+            >
+            <JustButtonLoading type="secondary"
+              >点击切换loading的次级按钮</JustButtonLoading
+            >
+            <JustButtonLoading type="revert"
+              >点击切换loading的回退按钮</JustButtonLoading
+            >
+            <!-- component event should use emit, default event like 'click' may block -->
+          </div>
+        </div>
+      </div>
     </div>
-
-    <div class="btn-disable-wrapper" v-if="type == 'btnDisable'">
-      <div>
-        <ButtonPrimary disabled>默认按钮</ButtonPrimary>
-        <ButtonPrimary type="primary" disabled>主要按钮</ButtonPrimary>
-        <ButtonPrimary type="secondary" disabled>次级按钮</ButtonPrimary>
-        <ButtonPrimary type="revert" disabled>回退按钮</ButtonPrimary>
-        <ButtonPrimary type="warning" disabled>警告按钮</ButtonPrimary>
-        <ButtonPrimary type="danger" disabled>危险按钮</ButtonPrimary>
-      </div>
-
-      <div>
-        <ButtonPrimary plain disabled>朴素按钮</ButtonPrimary>
-        <ButtonPrimary type="primary" plain disabled>主要按钮</ButtonPrimary>
-        <ButtonPrimary type="secondary" plain disabled>成功按钮</ButtonPrimary>
-        <ButtonPrimary type="revert" plain disabled>信息按钮</ButtonPrimary>
-        <ButtonPrimary type="warning" plain disabled>警告按钮</ButtonPrimary>
-        <ButtonPrimary type="danger" plain disabled>危险按钮</ButtonPrimary>
-      </div>
-    </div>
-
     <div class="input-wrapper" v-if="type == 'input'">
       <div class="input_test">
         <!-- <MyInput></MyInput> -->
-        <my-input v-model="value"></my-input>
-        <my-input v-model="value" icon="search"></my-input>
-        <my-input disabled></my-input>
-        <my-input showPassword></my-input>
-        <my-input clearable></my-input>
-        <my-input v-model="value1" type="textarea"></my-input>
-        <my-input readonly placeholder="输入"></my-input>
-        <my-input placeholder="输入"></my-input>
-        <my-input placeholder="输入" type="textarea"></my-input>
+        <JustInput v-model="value"></JustInput>
+        <JustInput v-model="value" icon="search"></JustInput>
+        <JustInput disabled></JustInput>
+        <JustInput showPassword></JustInput>
+        <JustInput clearable></JustInput>
+        <JustInput v-model="value1" type="textarea"></JustInput>
+        <JustInput readonly placeholder="输入"></JustInput>
+        <JustInput placeholder="输入"></JustInput>
+        <JustInput placeholder="输入" type="textarea"></JustInput>
       </div>
     </div>
     <div class="avatar-wrapper" v-if="type == 'avatar'">
       <div class="avatar_test">
         <div class="c1">
-          <avatar :src="imgUrl" size="small" alt="小头像" />
-          <avatar :src="imgUrl" size="default" />
-          <avatar :src="imgUrl" size="large" />
-          <avatar :src="imgUrl" :size="100" isRotated="isRotated" />
+          <JustAvatar :src="imgUrl" size="small" alt="小头像" />
+          <JustAvatar :src="imgUrl" size="default" />
+          <JustAvatar :src="imgUrl" size="large" />
+          <JustAvatar :src="imgUrl" :size="100" isRotated="isRotated" />
         </div>
 
         <div class="c2">
-          <avatar :src="imgUrl" size="small" shape="square" />
-          <avatar :src="imgUrl" size="default" shape="square" />
-          <avatar :src="imgUrl" size="large" shape="square" />
-          <avatar :src="imgUrl" :size="100" shape="square" />
+          <JustAvatar :src="imgUrl" size="small" shape="square" />
+          <JustAvatar :src="imgUrl" size="default" shape="square" />
+          <JustAvatar :src="imgUrl" size="large" shape="square" />
+          <JustAvatar :src="imgUrl" :size="100" shape="square" />
         </div>
 
         <div class="c3">
-          <avatar size="small">Hi</avatar>
-          <avatar size="default">Hi</avatar>
-          <avatar size="large">Hi</avatar>
-          <avatar :size="100">Hi</avatar>
+          <JustAvatar size="small">Hi</JustAvatar>
+          <JustAvatar size="default">Hi</JustAvatar>
+          <JustAvatar size="large">Hi</JustAvatar>
+          <JustAvatar :size="100">Hi</JustAvatar>
         </div>
 
         <div class="c4">
-          <avatar size="small" shape="square">Hi</avatar>
-          <avatar size="default" shape="square">Hi</avatar>
-          <avatar size="large" shape="square">Hi</avatar>
-          <avatar :size="100" shape="square">Hi</avatar>
+          <JustAvatar size="small" shape="square">Hi</JustAvatar>
+          <JustAvatar size="default" shape="square">Hi</JustAvatar>
+          <JustAvatar size="large" shape="square">Hi</JustAvatar>
+          <JustAvatar :size="100" shape="square">Hi</JustAvatar>
         </div>
 
         <div class="c5">
-          <avatar size="small" color="red">Hi</avatar>
-          <avatar size="default" color="white" background-color="gray"
-            >Hi</avatar
+          <JustAvatar size="small" color="red">Hi</JustAvatar>
+          <JustAvatar size="default" color="white" background-color="gray"
+            >Hi</JustAvatar
           >
-          <avatar size="large" background-color="gray">Hi</avatar>
-          <avatar :size="100" background-color="gray">Hi</avatar>
-        </div>
-      </div>
-    </div>
-    <div>
-      <div class="btn-shape-wrapper" v-if="type == 'btnShape'">
-        <div>
-          <ButtonPrimary round="square">方角</ButtonPrimary>
-          <ButtonPrimary round="round">圆角</ButtonPrimary>
-          <ButtonPrimary round="other1">平行四边形</ButtonPrimary>
-          <ButtonPrimary round="other2">椭圆</ButtonPrimary>
-          <ButtonPrimary round="other3">平行四边形</ButtonPrimary>
-          <ButtonPrimary round="other4">活力标题</ButtonPrimary>
-        </div>
-      </div>
-      <div class="btn-size-wrapper" v-if="type == 'btnSize'">
-        <div>
-          <ButtonPrimary type="primary" size="S">主要按钮</ButtonPrimary>
-          <ButtonPrimary type="primary" size="M">主要按钮</ButtonPrimary>
-          <ButtonPrimary type="primary" size="L">主要按钮</ButtonPrimary>
-        </div>
-
-        <div>
-          <ButtonPrimary type="secondary" size="S">次级按钮</ButtonPrimary>
-          <ButtonPrimary type="secondary" size="M">次级按钮</ButtonPrimary>
-          <ButtonPrimary type="secondary" size="L">次级按钮</ButtonPrimary>
-        </div>
-
-        <div>
-          <ButtonPrimary type="revert" size="S">回退按钮</ButtonPrimary>
-          <ButtonPrimary type="revert" size="M">回退按钮</ButtonPrimary>
-          <ButtonPrimary type="revert" size="L">回退按钮</ButtonPrimary>
-        </div>
-      </div>
-      <div class="btn-loading-wrapper" v-if="type == 'btnLoading'">
-        <div style="display: flex">
-          <!--防止点击后更换位置，需要在父级块状元素处增加style="display: flex;"-->
-          <ButtonLoading type="primary"
-            >点击切换loading的主要按钮</ButtonLoading
-          >
-          <ButtonLoading type="secondary"
-            >点击切换loading的次级按钮</ButtonLoading
-          >
-          <ButtonLoading type="revert">点击切换loading的回退按钮</ButtonLoading>
-
-          <!-- <ButtonPrimary @click="switchloading" :loading="!isLoading" type="primary"
-        >点击切换loading的主要按钮</ButtonPrimary
-      >
-      
-      <ButtonPrimary  @click="switchloading" :loading="!isLoading" type="secondary"
-        >点击切换loading的次级按钮</ButtonPrimary
-      >
-     
-      <ButtonPrimary @click="switchloading" :loading="!isLoading" type="revert"
-        >点击切换loading的回退按钮</ButtonPrimary
-      > -->
-          <!-- component event should use emit, default event like 'click' may block -->
+          <JustAvatar size="large" background-color="gray">Hi</JustAvatar>
+          <JustAvatar :size="100" background-color="gray">Hi</JustAvatar>
         </div>
       </div>
     </div>
 
-    <div class="dropselect-wrapper" v-if="type == 'dropselect'">
-      <DropSelect type="multiple" />
-      <DropSelect type="multiple" disabled />
-      <DropSelect type="multiple-search" />
+    <div class="dropselect-wrapper">
+      <div v-if="type == 'dropselect'">
+        <JustDropSelect type="multiple" />
+        <JustDropSelect type="multiple" disabled />
+        <JustDropSelect type="multiple-search" />
 
-      <DropSelect type="single" />
-      <DropSelect type="single" disabled />
+        <JustDropSelect type="single" />
+        <JustDropSelect type="single" disabled />
 
-      <DropSelect type="single-group" />
-      <DropSelect type="single-description" />
-      
-      <DropSelect type="single-cascader" />
-      <DropSelect type="single-linkage" />
+        <JustDropSelect type="single-group" />
+        <JustDropSelect type="single-description" />
+
+        <JustDropSelect type="single-cascader" />
+        <JustDropSelect type="single-linkage" />
+      </div>
+      <div v-if="type == 'dropselect-single-group'">
+        <JustDropSelect type="single-group" />
+      </div>
+      <div v-if="type == 'dropselect-multiple'">
+        <JustDropSelect type="multiple" />
+        <JustDropSelect type="multiple" disabled />
+        <JustDropSelect type="multiple-search" />
+      </div>
+      <div v-if="type == 'dropselect-mutiple-search'">
+        <JustDropSelect type="multiple-search" />
+      </div>
+      <div v-if="type == 'dropselect-single-cascader'">
+        <JustDropSelect type="single-linkage" />
+      </div>
+      <div v-if="type == 'cascader'">
+        <JustDropSelect type="single-cascader" />
+        <JustDropSelect type="single-cascader" disabled />
+      </div>
     </div>
-    <div v-if="type == 'dropselect-single-group'">
-      <DropSelect type="single-group" />
-    </div>
-    <div v-if="type == 'dropselect-multiple'">
-      <DropSelect type="multiple" />
-      <DropSelect type="multiple" disabled />
-      <DropSelect type="multiple-search" />
-    </div>
-    <div v-if="type == 'dropselect-mutiple-search'">
-      <DropSelect type="multiple-search" />
-    </div>
-    <div v-if="type == 'dropselect-single-cascader'">
-      <DropSelect type="single-linkage" />
-    </div>
-    <div v-if="type == 'cascader'">
-      <DropSelect type="single-cascader" />
-      <DropSelect type="single-cascader" disabled />
-    </div>
+    
   </Space>
 </template>
 
@@ -214,12 +212,12 @@ nav {
 
 <script lang="ts" setup>
 import { ref, onMounted } from 'vue';
-import ButtonPrimary from './views/button/button.vue';
-import ButtonLoading from './views/button/loadingbutton.vue';
-import avatar from './views/avatar/avatar.vue';
+import JustButton from './views/button/button.vue';
+import JustButtonLoading from './views/button/loadingbutton.vue';
+import JustAvatar from './views/avatar/avatar.vue';
 import imgUrl from './assets/image/avatar.png';
-import DropSelect from './views/dropselect/dropselect.vue';
-import MyInput from './views/input/input.vue';
+import JustDropSelect from './views/dropselect/dropselect.vue';
+import JustInput from './views/input/input.vue';
 
 const value = ref('请输入');
 const value1 = ref('输入文本');
