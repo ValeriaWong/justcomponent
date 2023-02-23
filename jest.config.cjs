a88f9f432f162ecc7926d236914f79fee7003108
ValeriaWong <763717425@qq.com>
2023-02-23 19:50:44 +0800

    chore: (testreport)gitignore添加测试覆盖报告，不将其推送至仓库

--- jest.config.cjs
+++ jest.config.cjs
@@ -16,8 +16,8 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   // 测试文件
-  testMatch: ['<rootDir>/src/__tests__/unit/**/*.spec.(ts|tsx|js)'],
-  // testMatch: ['<rootDir>/src/__tests__/unit/**/*.spec.(ts|tsx|js)','<rootDir>/src/__tests__/performance/*.perf.spec.(ts|tsx|js)'],
+  // testMatch: ['<rootDir>/src/__tests__/unit/**/*.spec.(ts|tsx|js)'],
+  testMatch: ['<rootDir>/src/__tests__/unit/**/*.spec.(ts|tsx|js)','<rootDir>/src/__tests__/performance/*.perf.spec.(ts|tsx|js)'],
   coverageDirectory: 'coverage',
   testPathIgnorePatterns: ['/node_modules/'],
   // eslint-disable-next-line no-dupe-keys
